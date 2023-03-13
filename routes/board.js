14a9fe4a133f9716f2ae1294cd33b9e0d0e53b08
김정혁 <jack1150@naver.com>
2023-03-13 15:43:00 +0900

    delete method

--- routes/board.js
+++ routes/board.js
@@ -44,9 +44,37 @@ router.post('/write', (req, res) => {
 });
 
 // 글 수정
-router.get('/modify/:title', (req, res) => {});
-router.post('/modify/:title', (req, res) => {});
+router.get('/modify/:title', (req, res) => {
+  const arrIndex = ARTICLE.findIndex((article) => {
+    req.params.title === article.title;
+    return req.params.title === article.title;
+  });
+  const selectedArticle = ARTICLE[arrIndex];
+  res.render('board_modify', { selectedArticle });
+});
+router.post('/modify/:title', (req, res) => {
+  if (req.body.title && req.body.content) {
+    const arrIndex = ARTICLE.findIndex((article) => {
+      article.title === req.params.title;
+      return article.title === req.params.title;
+    });
+    ARTICLE[arrIndex].title = req.body.title;
+    ARTICLE[arrIndex].content = req.body.content;
+    res.redirect('/board');
+  } else {
+    const err = new Error('폼 입력 확인하세요');
+    err.statusCode = 404;
+    throw err;
+  }
+});
 
 // 글 삭제
-router.delete('/delete/:title', (req, res) => {});
+router.delete('/delete/:title', (req, res) => {
+  const arrIndex = ARTICLE.findIndex((article) => {
+    article.title === req.params.title;
+    return article.title === req.params.title;
+  });
+  ARTICLE.splice(arrIndex, 1);
+  res.send('삭제 완료');
+});
 module.exports = router;
