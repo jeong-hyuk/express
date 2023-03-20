6f76263f68e7c29f17898426d35407f306def371
김정혁 <jack1150@naver.com>
2023-03-20 15:40:53 +0900

    mongoDB 설정

---
+++ controllers/mongo.js
@@ -0,0 +1,25 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri =
+  'mongodb+srv://jack11500808:ne42170808~@cluster0.odkhywu.mongodb.net/?retryWrites=true&w=majority';
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+client.connect((err) => {
+  const test = client.db('kdt5').collection('test');
+  test.deleteMany({}, (deleteErr, deleteResult) => {
+    if (deleteErr) throw deleteErr;
+    console.log(deleteResult);
+    test.insertOne(
+      {
+        name: 'jack',
+        nickname: 'pig',
+      },
+      (insertErr, insertResult) => {
+        console.log(insertResult);
+        client.close();
+      },
+    );
+  });
+});
